de22c444aff324ee49e5dad1397dd3581db84dca
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-03-27 10:36:04 -0300

    Alterando mensagem de retorno na rota de testes e adicionando rota de criar cidade

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { CidadesController } from "../controllers/index";
+
 const router = Router();
 
 router.get("/", (req, res) => {
   res.status(StatusCodes.OK);
-  res.json({ message: "Hello World" });
+  res.json({ message: "Servidor rodando" });
 });
+
+router.post("/cidades", CidadesController.create);
 
 export { router };
