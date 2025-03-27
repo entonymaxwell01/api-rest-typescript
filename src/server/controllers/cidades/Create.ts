417614f5e25147d308d16d12e8311946e23bfdc3
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-03-27 10:36:37 -0300

    Criando controller de criação de cidades

---
+++ src/server/controllers/cidades/Create.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
+
+interface ICidade {
+  nome: string;
+  estado: string;
+  pais: string;
+}
+
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object({
+  nome: yup.string().required().min(3),
+  estado: yup.string().min(3).required(),
+  pais: yup.string().min(3).required(),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade | undefined = undefined;
+
+  try {
+    await bodyValidation.validate(req.body);
+    res
+      .status(StatusCodes.CREATED)
+      .json({ validatedData, message: "Cidade criada com sucesso" });
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: yupError.message,
+      },
+    });
+  }
+};
