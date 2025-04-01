bc03c57c64664a59a248fab4a262444ca03b7180
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-04-01 11:31:17 -0300

    exportando métodos de get, update e delete de cidades

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,7 +1,19 @@
 import { create } from "./Create";
 import { createValidation } from "./Create";
+import { getAllValidation, getAll } from "./GetAll";
+import { getByIdValidation, getById } from "./GetById";
+import { update, updateValidation } from "./Update";
+import { deleteValidation, deleteCidade } from "./Delete";
 
 export const CidadesController = {
   create,
   createValidation,
+  getAll,
+  getAllValidation,
+  getById,
+  getByIdValidation,
+  update,
+  updateValidation,
+  deleteCidade,
+  deleteValidation,
 };
